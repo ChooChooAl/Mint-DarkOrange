7e200c932a2ace2b2b6a1ee73a6a2e14e87fa281
ChooChooAl <26556059+ChooChooAl@users.noreply.github.com>
2024-11-11 16:56:30 -0500

    Initial upload of files

---
+++ cinnamon/notify-test.sh
@@ -0,0 +1,5 @@
+notify-send  -i /home/alan/.icons/Icons/notification.png "Ask The Tech Guys" "Tech experts Leo Laporte and Mikah Sargent team up to answer all of your burning tech questions. From the latest gadgets to tried-and-true tech tips, Leo & Mikah have the knowledge and expertise to help you get the most out of your technology. So whether you're a tech novice or a seasoned pro, join Leo and Mikah each week for a fun and informative look at the world of technology.\n\nCall in with your live questions from 11 am-2 pm PT every Sunday at call.twit.tv. Get ready to level up your tech game!\n\nRecords live Sunday at 2:00pm Eastern / 11:00am Pacific / 19:00 UTC."
+sleep 7
+notify-send  -i /home/alan/.icons/Icons/notification.png "Header" "Short comment."
+sleep 7
+notify-send  -i /home/alan/.icons/Icons/notification.png "Header only"
