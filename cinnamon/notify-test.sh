71b9ec131b64a43a86a0edc6bc902f039bcfa4d6
ChooChooAl <26556059+ChooChooAl@users.noreply.github.com>
2024-12-03 17:38:54 -0500

    New thumbnails, compensate for Mint 22 not rendering large icons smaller

--- cinnamon/notify-test.sh
+++ cinnamon/notify-test.sh
@@ -1,5 +1,6 @@
-notify-send  -i /home/alan/.icons/Icons/notification.png "Ask The Tech Guys" "Tech experts Leo Laporte and Mikah Sargent team up to answer all of your burning tech questions. From the latest gadgets to tried-and-true tech tips, Leo & Mikah have the knowledge and expertise to help you get the most out of your technology. So whether you're a tech novice or a seasoned pro, join Leo and Mikah each week for a fun and informative look at the world of technology.\n\nCall in with your live questions from 11 am-2 pm PT every Sunday at call.twit.tv. Get ready to level up your tech game!\n\nRecords live Sunday at 2:00pm Eastern / 11:00am Pacific / 19:00 UTC."
-sleep 7
-notify-send  -i /home/alan/.icons/Icons/notification.png "Header" "Short comment."
-sleep 7
-notify-send  -i /home/alan/.icons/Icons/notification.png "Header only"
+#!/usr/bin/bash
+notify-send  -i ${HOME}/.icons/Icons/Letters/yellow1.png "Chase Elliott" "He drives the No. 9 Chevrolet for Hendrick Motorsports. Elliott won his first Cup Series championship in 2020, his fifth full season in Cup, by capturing his fifth win of the season at Phoenix Raceway. In all, he has 19 career Cup Series wins."
+sleep 4
+notify-send  -i ${HOME}/.icons/Icons/Letters/yellow2.png "Header" "Short comment."
+sleep 4
+notify-send  -i ${HOME}/.icons/Icons/Letters/yellow3.png "Header only"
