90df0e47de4b923a63d5cf3201689ec279aff87c
ChooChooAl <26556059+ChooChooAl@users.noreply.github.com>
2025-11-18 18:11:30 -0500

    new thumbnails - easier to see.

--- cinnamon/notify-test.sh
+++ cinnamon/notify-test.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 notify-send  -i ${HOME}/.icons/Icons/Letters/yellow1.png "Chase Elliott" "He drives the No. 9 Chevrolet for Hendrick Motorsports. Elliott won his first Cup Series championship in 2020, his fifth full season in Cup, by capturing his fifth win of the season at Phoenix Raceway. In all, he has 19 career Cup Series wins."
 sleep 4
 notify-send  -i ${HOME}/.icons/Icons/Letters/yellow2.png "Header" "Short comment."
